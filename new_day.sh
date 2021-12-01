3480c9c4abcfbd8219d6af8af3ac7b8ef7a75271
Josep Lluis Giralt D'Lacoste <pep.g.dlacoste@erlang-solutions.com>
2021-12-01 08:24:58 +0000

    Better formatting

--- new_day.sh
+++ new_day.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DAY=$(printf "%02d" $1)
 DIR=day_$DAY
-SESSION_ID=${2:-53616c7465645f5f84dfb53cd64d9fa4741756fa6b70dec67c17c3f47a1d9858f17cb689cb1efa5af15a56f11f6e917b}
+SESSION_ID=${2:-$AOC_SESSION_ID}
 
 echo $DAY
 echo $DIR
@@ -11,22 +11,29 @@ create ()
 {
 cat > $1 <<EOF
 -module($2).
+
 -export([main/1, readlines/1]).
+
 main(Input) ->
     Lines = readlines(Input),
     erlang:display(Lines).
+
 readlines(FileName) ->
     {ok, Data} = file:read_file(FileName),
     BinSplit = binary:split(Data, [<<"\n">>], [global]),
     parse(BinSplit, []).
+
 % ocurrences(List, El, string) -> length([X || X <- List, [X] =:= El]),
 % even_print([]) ->
 %     [];
+
 % even_print([H | T]) when H rem 2 /= 0 ->
 %     even_print(T);
+
 % even_print([H | T]) ->
 %     io:format("printing: ~p~n", [H]),
 %     [H | even_print(T)].
+
 parse([<<>>], Buffer) ->
     lists:reverse(Buffer);
 parse([<<H/binary>> | T], Buffer) ->
