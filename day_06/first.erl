2cfe803997325aa1ee87f787c7d52604db3b60b8
Josep Lluis Giralt D'Lacoste <pep.g.dlacoste@erlang-solutions.com>
2021-12-06 19:44:54 +0000

    Day six part one

--- day_06/first.erl
+++ day_06/first.erl
@@ -2,6 +2,10 @@
 
 -export([main/1]).
 
+-record(lantern_fish, {
+    current_day = 8 :: non_neg_integer()
+}).
+
 main(Input) ->
     Lines = readlines(Input),
     erlang:display(Lines).
@@ -9,30 +13,67 @@ main(Input) ->
 readlines(FileName) ->
     {ok, Data} = file:read_file(FileName),
     BinSplit = binary:split(Data, [<<"\n">>, <<",">>], [global]),
-    io:format("1~p", [BinSplit]),
     BinSplitCleanned = lists:delete(<<>>, BinSplit),
-    io:format("2~p", [BinSplitCleanned]),
-    parse(BinSplit, []).
+    InitialLanternFishs = parse(BinSplitCleanned, []),
+    InitialState = #{newborns => 0, lantern_fish_list => InitialLanternFishs},
+    Days = lists:seq(1, 256),
+    #{lantern_fish_list := FinalLanternFishList} =
+        lists:foldl(
+            fun(_Day, #{lantern_fish_list := LanternFishList}) ->
+                next_day(LanternFishList, #{newborns => 0, lantern_fish_list => []})
+            end,
+            InitialState,
+            Days
+        ),
+    length(FinalLanternFishList).
 
-% split(Binary, By) ->
-%     list_to_tuple(binary:split(Binary, By, [global])).
+append_lintern_fishs(List, none) -> List;
+append_lintern_fishs(List, NewBorns) -> lists:append(List, NewBorns).
 
-% build_acc(Length) ->
-%     [{0, 0} || _X <- lists:seq(1, Length)].
+new_borns(N) when N > 0 ->
+    [#lantern_fish{} || _X <- lists:seq(1, N)];
+new_borns(_) ->
+    none.
 
-% parse_integer(Bin) ->
-%    {Number, <<>>} = string:to_integer(Bin),
-%    Number.
+next_day([], #{lantern_fish_list := LanternFishList, newborns := NewBorns} = NLF) ->
+    WithNewBorns = lists:reverse(append_lintern_fishs(LanternFishList, new_borns(NewBorns))),
+    NLF#{lantern_fish_list := WithNewBorns};
+next_day(
+    [LanternFish | T],
+    #{
+        newborns := NewBorns,
+        lantern_fish_list := LanternFishList
+    } = NLF
+) ->
+    NewLanternFishs =
+        case new_day(LanternFish) of
+            {true, NewLanternFish} ->
+                NLF#{
+                    newborns := NewBorns + 1,
+                    lantern_fish_list := [NewLanternFish | LanternFishList]
+                };
+            {false, NewLanternFish} ->
+                NLF#{
+                    lantern_fish_list := [NewLanternFish | LanternFishList]
+                }
+        end,
+    next_day(T, NewLanternFishs).
+
+new_day(#lantern_fish{current_day = CurrentDay} = Lf) when
+    CurrentDay < 9, CurrentDay > 0
+->
+    {false, Lf#lantern_fish{current_day = CurrentDay - 1}};
+new_day(#lantern_fish{current_day = 0} = Lf) ->
+    {true, Lf#lantern_fish{current_day = 6}}.
+
+is_first_day(8) ->
+    true;
+is_first_day(_) ->
+    false.
 
 parse([], Buffer) ->
-    lists:reverse(Buffer);
+    Buffer;
 parse([<<H/binary>> | T], Buffer) ->
-    % {First, Second} = split(H, [<<" ">>]),
-    case H of
-        <<>> ->
-            parse(T, [Buffer]);
-        _ ->
-            io:format("3~p", [H]),
-            {Number, <<>>} = string:to_integer(H),
-            parse(T, [Number | Buffer])
-    end.
+    {Day, <<>>} = string:to_integer(H),
+    LanternFish = #lantern_fish{current_day = Day, is_first_day = is_first_day(Day)},
+    parse(T, [LanternFish | Buffer]).
